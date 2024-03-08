65bb2c7a69ff378bedf9e3ba7c513498c062c9bb
Yan Yashyn <yan.yashyn.s@gmail.com>
2024-03-08 19:20:50 +0200

    Entry points updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Login, Home, Product } from "./domains";
+import { AuthProvider } from "./providers";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Routes>
+        <Route exact index element={<Login />} />
+        <Route exact path="products" element={<Home />} />
+        <Route exact path="products/:id" element={<Product />} />
+      </Routes>
+    </AuthProvider>
   );
 }
 
